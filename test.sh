9360bcc2b4a67485deb5c6ead07f2d9268f3d733
Zachary Sunberg <zachary.sunberg@colorado.edu>
2021-05-01 16:32:58 -0600

    added rho pomdps

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-biber --tool --validate-datamodel references.bib | grep -v Datamodel | grep --color 'Duplicate\|ERROR\|$'
+biber --tool references.bib | grep --color 'Duplicate\|ERROR\|$'
 # biber --tool --validate-datamodel references.bib | grep --color 'Duplicate\|ERROR\|$'
