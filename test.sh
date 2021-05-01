d01b6a6d58a501dedae291256f522d8a87caeb85
Zachary Sunberg <zachary.sunberg@colorado.edu>
2021-05-01 12:45:48 -0600

    tested and removed duplicates

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-biber --tool --validate-datamodel references.bib
+biber --tool --validate-datamodel references.bib | grep -v Datamodel | grep --color 'Duplicate\|ERROR\|$'
+# biber --tool --validate-datamodel references.bib | grep --color 'Duplicate\|ERROR\|$'
